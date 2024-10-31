05d02df0537f9df72a299709b9dcb341056bcf5c
DrCoder <shakil.7878@gmail.com>
2024-10-31 19:41:15 +0530

    updated server.js port fix issue

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', process.env.PORT || 1337), // Use Render's PORT if available
+  host: '0.0.0.0', // Explicitly set the host to 0.0.0.0 as Render requires
+  port: parseInt(process.env.PORT, 10) || 1337, // Use Render's PORT or default to 1337
   app: {
     keys: env.array('APP_KEYS'),
   },
