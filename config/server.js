7efec2fd321351e08228bce792084b171c3f1d90
DrCoder <shakil.7878@gmail.com>
2024-10-31 22:24:53 +0530

    updated server.js port fix issue 6

--- config/server.js
+++ config/server.js
@@ -1,10 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: parseInt(process.env.PORT, 10) || 1337, // Use Render's `PORT` or fallback to 1337 for local development
+  port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    keys: env.array('APP_KEYS')
   },
 });
